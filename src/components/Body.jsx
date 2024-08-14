44e1c1478d56f70358dfa26da3964c2337335357
Ananya Raj <ananyam748@gmail.com>
2024-08-14 20:49:15 +0530

    render

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <h2>header</h2>
+  )
+}
+
+export default Body
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h4>header</h4>
+  )
+}
+
+export default Header
